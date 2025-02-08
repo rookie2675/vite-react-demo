362231c05bd64f5d30c634a9e531624dd6583e4e
Ricardo Almeida <ricardo2675@proton.me>
2025-02-08 22:42:24 +0000

    Separated the connect four board cell from parent board component

--- src/features/games/connect-four/ConnectFourBoard.tsx
+++ src/features/games/connect-four/ConnectFourBoard.tsx
@@ -1,16 +1,20 @@
 import styles from "./ConnectFourBoard.module.css";
+import ConnectFourBoardCell from "./ConnectFourBoardCell";
+
+export type Token = "red" | "yellow" | null;
 
 const rows = 6;
 const columns = 7;
 
-function createGrid(): React.JSX.Element[][] {
+function createGrid(currentToken: Token): React.JSX.Element[][] {
     const array: React.JSX.Element[][] = [];
 
     for (let rowIndex = 0; rowIndex < rows; rowIndex++) {
         const row: React.JSX.Element[] = [];
 
         for (let columnIndex = 0; columnIndex < columns; columnIndex++) {
-            row.push(<td className={styles.square} key={`${rowIndex.toString()}${columnIndex.toString()}`}></td>);
+            const key = `${rowIndex.toString()}${columnIndex.toString()}`;
+            row.push(<ConnectFourBoardCell key={key} token={currentToken} />);
         }
 
         array.push(row);
@@ -20,7 +24,7 @@ function createGrid(): React.JSX.Element[][] {
 }
 
 export default function ConnectFourBoard(): React.JSX.Element {
-    const grid = createGrid();
+    const grid = createGrid("yellow");
 
     return (
         <table className={styles.table}>
---
+++ src/features/games/connect-four/ConnectFourBoardCell.tsx
@@ -0,0 +1,20 @@
+import styles from "./ConnectFourBoardCell.module.css";
+import { Token } from "./ConnectFourBoard";
+
+interface Props {
+    readonly token: Token;
+}
+
+export default function ConnectFourBoardCell(props: Props): React.JSX.Element {
+    let style = "";
+
+    if (props.token === "red") {
+        style = styles.redCell;
+    } else if (props.token === "yellow") {
+        style = styles.yellowCell;
+    } else {
+        style = styles.whiteCell;
+    }
+
+    return <td className={style}></td>;
+}
