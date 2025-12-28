fc337977ff2d4d4a34aff50c39aa41a55d0997e3
Ricardo Almeida <ricardo2675@proton.me>
2025-12-28 13:02:55 +0000

    Improved controlled number input component

--- src/components/Inputs/ControlledNumberInputComponent.tsx
+++ src/components/Inputs/ControlledNumberInputComponent.tsx
@@ -1,16 +1,19 @@
+import styles from "./ControlledNumberInputComponent.module.css";
+
 interface Props {
     readonly min: number;
     readonly max: number;
     readonly value: number;
+    readonly disabled?: boolean;
     readonly setValue: (value: number) => void;
 }
 
 export default function ControlledNumberInputComponent(props: Props): React.JSX.Element {
     function handleChange(event: React.ChangeEvent<HTMLInputElement>): void {
-        const inputValue = parseInt(event.target.value) || props.min;
-        const clampedValue = Math.min(props.max, Math.max(props.min, inputValue));
+        const inputValue: number = parseInt(event.target.value) || props.min;
+        const clampedValue: number = Math.min(props.max, Math.max(props.min, inputValue));
         props.setValue(clampedValue);
     }
 
-    return <input type="number" min={props.min} max={props.max} value={props.value} onChange={handleChange} />;
+    return <input className={styles.input + (props.disabled ? ' ' + styles.disabled : '')} type="number" min={props.min} max={props.max} value={props.value} onChange={handleChange} disabled={props.disabled} />;
 }
